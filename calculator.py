fba75fb456b7e7639e09e05fa9f46e9892642f3e
Your Name <zfaruque@uci.edu>
2025-11-17 23:46:21 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,18 @@
+# https://github.com/zfaruque/lab11-ZF-LB/blob/main/calculator.py
+# partner 1: Zibran Faruque
+# partner 2: Lorenzo Baino
 """
 calculator.py
 - Defines functions used to create a simple calculator
 
 One function per operation, in order.
 """
+import math
+
 # First example
-def add(a, b): 
-    pass
-
-
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b): 
+    return a * b
