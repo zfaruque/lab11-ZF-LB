e2f9cd049d332d32e05567f4ab10c24af02fd7c1
Your Name <zfaruque@uci.edu>
2025-11-18 16:57:01 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -17,7 +17,7 @@ def hypotenuse(a, b):
     return math.hypot(a, b)
 def add(a, b):
     return a + b
-def sub(a, b):
+def subtract(a, b):
     return a - b
 def mul(a, b):
     return a * b
@@ -26,7 +26,7 @@ def div(a, b):
         raise ZeroDivisionError
     else:
         return b / a
-def log(a, b):
+def logarithm(a, b):
     if a == 0:
         raise ValueError
     else:
