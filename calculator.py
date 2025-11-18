fe2096b1f78ac775566b4c03144bdc1a19c7dbb1
zo-z0o <lorenzovbaino@gmail.com>
2025-11-17 23:19:45 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
+# https://github.com/zfaruque/lab11-ZF-LB/blob/main/calculator.py
+# partner 1: Zibran Faruque
+# partner 2: Lorenzo Baino
 """
 calculator.py
 - Defines functions used to create a simple calculator
